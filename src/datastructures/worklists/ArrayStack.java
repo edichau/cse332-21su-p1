bca020e094db62c09a1250b95bcb761009c95a5e
Lauren <lecavs91@msn.com>
2021-06-28 20:45:14 -0700

    ArrayStack implemented using E[]. Passes all tests.

--- src/datastructures/worklists/ArrayStack.java
+++ src/datastructures/worklists/ArrayStack.java
@@ -3,38 +3,67 @@ package datastructures.worklists;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.LIFOWorkList;
 
+import java.util.NoSuchElementException;
+
 /**
  * See cse332/interfaces/worklists/LIFOWorkList.java
  * for method specifications.
  */
 public class ArrayStack<E> extends LIFOWorkList<E> {
+    int size;
+    E[] workList;
 
     public ArrayStack() {
-        throw new NotYetImplementedException();
+        this.size = 0;
+        workList= (E[])new Object[10];
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+
+        if(size == workList.length) { //out of space in workList
+            //create new array double the size and copy values of current workList into newWorkList
+            //since we double the size every time we run out of space, our amortized time to add ends up being O(1)
+
+            int newArrayLength= workList.length * 2;
+            E[] newWorkList = (E[]) new Object[newArrayLength];
+            for(int i = 0; i < size; i++) {
+                newWorkList[i] = workList[i];
+            }
+            newWorkList[size] = work;
+            this.workList = newWorkList;
+        } else {
+            workList[size] = work;
+        }
+        size++;
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if (!super.hasWork()) {
+            throw new NoSuchElementException();
+        }
+        return workList[size - 1];
     }
 
     @Override
     public E next() {
-        throw new NotYetImplementedException();
+        if (!super.hasWork()) {
+            throw new NoSuchElementException();
+        }
+        E work = workList[size - 1];
+        size--;
+        return work;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return size;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        size = 0;
+        this.workList= (E[])new Object[10];
     }
 }
