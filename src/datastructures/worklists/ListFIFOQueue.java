f6af0f06bb9f81583f10901f516bd570dfd0c4c8
BuildTools <unconfigured@null.spigotmc.org>
2021-06-28 19:05:59 -0700

    added node to ListFIFOQueue.java

--- src/datastructures/worklists/ListFIFOQueue.java
+++ src/datastructures/worklists/ListFIFOQueue.java
@@ -2,15 +2,32 @@ package datastructures.worklists;
 
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.FIFOWorkList;
+import java.util.NoSuchElementException;
 
 /**
  * See cse332/interfaces/worklists/FIFOWorkList.java
  * for method specifications.
  */
 public class ListFIFOQueue<E> extends FIFOWorkList<E> {
-    
+
+    private class Node {
+        public E data;
+        public Node next;
+
+        public Node(E data) {
+            this(data, null);
+        }
+
+        public Node(E data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
     public ListFIFOQueue() {
-        throw new NotYetImplementedException();
+        Node front = null;
+        Node back = null;
+        int size = 0;
     }
 
     @Override
